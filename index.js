eca98a8fdef4cb78835335fe73ee1c3af2f1bb66
amolinaro <amolinaro@payall.com.ve>
2022-12-12 09:07:30 -0400

    acomodado

--- index.js
+++ index.js
@@ -10,32 +10,13 @@ app.post("/gecko/api/payall/payin/", (request, response) =>
     console.log(request.body)
     console.log("<<<<<Fin Peticion>>>>>" + (nro))
     
-    if(request.body.transaction_c2p.destination_bank_id == '0163' && request.body.transaction_c2p.trx_type == 'compra')
+    if(request.body)
     {
         setTimeout(() =>
         {
         response.status(400).send(
             {
-                "Merchant_Identify":
-                {
-                    "integratorId": "1",
-                    "merchantId": "150332",
-                    "terminalId": "1"
-                },
-                "transaction_c2p_response":
-                {
-                    "processing_date": "2019-09-12 03:07:53 VET",
-                    "trx_status": "approved",
-                    "trx_type": "compra",
-                    "payment_method": "c2p",
-                    "payment_reference": "0057718281656",
-                    "invoice_number": "113466",
-                    "amount": 1.00,
-                    "currency": "ves",
-                    "trx_internal_status": "0000",
-                    "authorization_code": "002166",
-                    "service_fee": "0"
-                }
+                
             }
             )
         }, timeout);
