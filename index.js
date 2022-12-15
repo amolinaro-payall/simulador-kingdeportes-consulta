3f5b25b018a1ebad491d9ca695c94da53b849907
amolinaro <amolinaro@payall.com.ve>
2022-12-15 15:36:55 -0400

    sera

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(Express.json());
 const timeout = 30000;
 let nro = 0;
 let envio = '{"resultados": 2,"items": [{"id": "13717","username": "adrian2005","email": "adri*****26@gmail.com","phone": "+580******9804","document": "25000000"},{"id": "28124","username": "manuel23","email": "manu*****ma@gmail.com","phone": "+580******9095","document": "25000000"}],"term": "04247539"}';
+const datos = JSON.parse(envio);
 app.post("/gecko/api/payall/term/", (request, response) =>
 {
     console.log("<<<<<Nueva Peticion:>>>>>" + (nro++))
@@ -12,50 +13,9 @@ app.post("/gecko/api/payall/term/", (request, response) =>
     console.log("<<<<<Fin Peticion>>>>>" + (nro))
     
     if(request.body.transaction_c2p.destination_bank_id != '0115' && request.body.transaction_c2p.trx_type == 'anulacion'){
-        response.status(200).json(
-            {
-                "resultados": 2,
-                "items": [
-                  {
-                    "id": "13717",
-                    "username": "adrian2005",
-                    "email": "adri*****26@gmail.com",
-                    "phone": "+580******9804",
-                    "document": "25000000"
-                  },
-                  {
-                    "id": "28124",
-                    "username": "manuel23",
-                    "email": "manu*****ma@gmail.com",
-                    "phone": "+580******9095",
-                    "document": "25000000"
-                  }
-                ],
-                "term": "04247539"
-              }
-        )
+        response.status(200).send(datos)
     }else{
-      response.status(200).json(
-        {
-          "resultados": 2,
-          "items": [{
-              "id": "13717",
-              "username": "adrian2005",
-              "email": "adri*****26@gmail.com",
-              "phone": "+580******9804",
-              "document": "25000000"
-            },
-            {
-              "id": "28124",
-              "username": "manuel13",
-              "email": "manu*****ma@gmail.com",
-              "phone": "+580******9095",
-              "document": "25000000"
-            }
-          ],
-          "term": "04247539"
-        }
-      )
+      response.status(200).send(datos)
       }
 })
 app.listen(puerto, () => console.log("Escuchando en el puerto: "+ puerto))
